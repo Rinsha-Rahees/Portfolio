2f308da0a1462ef7a272cd83f35835c8d215ec9c
RinshaRahees <104898673+RinshaRahees@users.noreply.github.com>
2023-10-30 06:04:00 +0530

    About Desc Done

--- my-portfolio/src/Header.js
+++ my-portfolio/src/Header.js
@@ -1,1 +1,35 @@
 import './Header.css';
+
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='headerContainer'>
+
+        {/* <header className="App-header"> */}
+
+        
+          <div className="headerMenu">
+            <div className="About">
+              <p className='text-xl font-medium text-white'>About</p>
+            </div>
+            <div className="Portfolio">
+              <p className='text-xl font-medium text-white'>Portfolio</p>
+            </div>
+            <div className="Contact">
+              <p className='text-xl font-medium text-white'>Contact</p>
+            </div>
+
+
+            {/* Responsive till Tablet view.
+             MOBILE VIEW - NOT RESPONSIVE : Change to menu items in mobile view later */}
+            
+          </div>
+
+        {/* </header> */}
+        
+    </div>
+  )
+}
+
+export default Header
