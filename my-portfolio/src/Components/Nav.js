ab14c357e1d9ae9783985cb58baf5f0f0a89a0c4
RinshaRahees <104898673+RinshaRahees@users.noreply.github.com>
2023-11-23 04:13:22 +0530

    Final design fix

--- my-portfolio/src/Components/Nav.js
+++ my-portfolio/src/Components/Nav.js
@@ -5,7 +5,7 @@ function Nav(props) {
     const { children, href } = props
     return (
         <nav>
-            <ul className='NavItems flex flex-row items-center'>
+            <ul className='NavItems flex flex-row items-center mt-5'>
             <hr className='w-8 float-left mr-2 invisible lg:visible'/><NavItems href={href}>{children}</NavItems>
             </ul>
         </nav>
