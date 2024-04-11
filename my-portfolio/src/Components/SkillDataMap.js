15941e6eda6fcabbce89798e3d8dddbe4c9b10b5
RinshaRahees <104898673+RinshaRahees@users.noreply.github.com>
2024-04-11 13:54:07 +0530

    Bug Fix: Skills container alignment

--- my-portfolio/src/Components/SkillDataMap.js
+++ my-portfolio/src/Components/SkillDataMap.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function SkillDataMap({ name, icon }) {
   return (
-    <div className="flex py-5">
+    <div className="flex justify-between sm:justify-start py-5">
       <h2 className="mr-4">{name}</h2>
       <img className="w-7 h-7" src={icon} alt="Skill Logo" />
     </div>
--- my-portfolio/src/Components/Skills.js
+++ my-portfolio/src/Components/Skills.js
@@ -9,7 +9,7 @@ function Skills() {
       <div className="WorkDesc flex flex-col flex-1 my-8">
         <Subtitle>SKILLS</Subtitle>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-4 text-blue-light font-medium">
+        <div className="grid grid-cols-1 w-2/3 self-center sm:w-full sm:self-start sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 mt-4 text-blue-light font-medium">
         {tools.map((tool) => (
             <SkillDataMap key={tool?.name} name={tool?.name} icon={tool?.src} />
         ))}
